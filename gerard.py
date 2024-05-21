78deec0c7fd4e962c79ac099a84ff849199c6147
GeriiGarcia <gerardgarciagros@gmail.com>
2024-05-21 09:41:08 +0200

    se ven las graficas de locos

--- gerard.py
+++ gerard.py
@@ -1,15 +1,12 @@
 import json
 import matplotlib.pyplot as plt
-from plots import *
-
 from datetime import datetime
 
 def loaders(): 
-    # Ruta al archivo JSON
-    json_file_scheduler = 'schedule.json'
-    json_file_record = '06_53_16.JSON'
-
     # Cargar los datos del archivo JSON
+    json_file_scheduler = 'schedule.json'
+    json_file_record = '08_31_01.JSON'
+
     with open(json_file_scheduler) as file:
         scheduler = json.load(file)
 
@@ -19,7 +16,6 @@ def loaders():
     return scheduler, test1
 
 def get_first_station(scheduler, linea, sentido, origen, t_inicio):
-    # seleccionar los "departure_time" de la linea y sentido seleccionados
     departure_times = []
     for item in scheduler:
         if item['route_short_name'] == linea and item['trip_headsign'] == sentido and item['stop_name'] == origen:
@@ -28,10 +24,8 @@ def get_first_station(scheduler, linea, sentido, origen, t_inicio):
                 departure_times.append(time)
             except ValueError:
                 continue
-    # Convertir t_inicio a datetime para poder hacer la comparación
+    
     t_inicio = datetime.combine(datetime.today(), t_inicio)
-
-    # Calcular la hora más cercana
     nearest_time = min(departure_times, key=lambda x: abs(datetime.combine(datetime.today(), x) - t_inicio))
 
     return nearest_time
@@ -51,36 +45,25 @@ def get_stop_ids_from_time(scheduler, linea, origen, sentido, timestamp):
     return stop_ids
 
 def get_stations(scheduler, origen, destino, sentido, linea, t_inicio):
-    # seleccionar los valores únicos de "stop_name" de la linea y sentido seleccionados
-    
     stops = {}
     for item in scheduler:
         if item['route_short_name'] == linea and item['trip_headsign'] == sentido:
             if item['stop_name'] not in stops.keys():
                 stops[item['stop_name']] = item['stop_sequence']
-    # ordenar por "stop_sequence"
+
     stations = sorted(stops, key=stops.get)
-    # eliminar las estaciones antes de la estación de origen
     stations = stations[stations.index(origen):stations.index(destino)+1]
     return stations
 
 def get_closest_time_vector(vectors, target_time):
     def time_difference(vector):
-        time_str = vector["departure_time"]  # Assuming the time is in the third position
+        time_str = vector["departure_time"]
         vector_time = datetime.strptime(time_str, "%H:%M:%S").time()
         return abs(datetime.combine(datetime.today(), vector_time) - datetime.combine(datetime.today(), target_time))
     
     closest_vector = min(vectors, key=time_difference)
     
     return closest_vector
-
-def print_connection_by_timestamp(data, timestamp):
-    for item in data:
-        if item[0]['time'] == timestamp:
-            print(f"Connection at timestamp {timestamp}: {item['connection']}")
-            return
-    print(f"No connection found for timestamp {timestamp}")
-
 
 def __main__():
     scheduler, travel = loaders()
@@ -92,8 +75,7 @@ def __main__():
 
     stations = get_stations(scheduler, origen, destino, sentido, linea, t_inicio)
     t_inicio = get_first_station(scheduler, linea, sentido, origen, t_inicio)
-    print("Hora de salida del tren:", t_inicio.strftime("%H:%M:%S")) # Hora a la que sale el tren
-    
+    #t_inicio = datetime.strptime("08:32:28", "%H:%M:%S").time()    # en caso de error por tiempo poner aqui uno q este dentro del JSON
     llista_trajecte = [[t_inicio.strftime("%H:%M:%S"), origen]]
 
     for i in range(len(stations)-1):
@@ -104,29 +86,36 @@ def __main__():
         origen = closest_vector["stop_name"]
         llista_trajecte.append([t_inicio.strftime("%H:%M:%S"), origen])
 
-    print("Lista de trayecto:", llista_trajecte) 
-    
-    values_list = [[entry['connection']] for entry in travel]
-    time_list = [[entry['time']] for entry in travel]
-    times_datetime = [datetime.strptime(time[0], "%H:%M:%S") for time in time_list]
+    days = []
+    time_list = []
+    values_list = []
+
+    for entry in travel["data"]:
+        days.append(entry['day'])
+        time_list.append(entry['time'])
+        values_list.append(entry['connection'])
 
     # Crear el gráfico
-    plt.figure(figsize=(10, 6))
-    plt.plot(times_datetime, values_list, marker='o', linestyle='-')
+    plt.figure(figsize=(14, 6))
+    plt.plot(time_list, values_list, marker='o', linestyle='-')
+
+    # Agregar el nombre de la estación en la hora correspondiente
+    for time, station in llista_trajecte:
+        plt.annotate(station, xy=(time, values_list[time_list.index(time)]), xytext=(5, 5), textcoords='offset points', ha='right')
 
     # Configurar etiquetas y título
-    plt.xlabel('Tiempo')
+    plt.xlabel('Tiempo y Estaciones')
     plt.ylabel('Valor')
-    plt.title('Valores en el Tiempo')
+    plt.title('Valores en el Tiempo y Estaciones')
 
-    # Rotar las etiquetas del eje x para mayor legibilidad
-    plt.xticks(rotation=45)
+    # Mostrar solo una de cada 10 etiquetas en el eje x
+    x_ticks = range(0, len(time_list), 20)
+    plt.xticks(x_ticks, [time_list[i] for i in x_ticks], rotation=45)
 
     # Mostrar el gráfico
     plt.grid(True)
     plt.tight_layout()
     plt.show()
-
 
 if __name__ == "__main__":
     __main__()
